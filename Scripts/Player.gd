0536468ec5f6ee3101d6e53bb3b7cd959dfc6837
Robo0890 <70237244+Robo0890@users.noreply.github.com>
2020-09-02 18:06:37 -0500

    wiohg

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,10 +2,10 @@ extends KinematicBody2D
 
 
 var velocity = Vector2()
-export var gravity = 56
-export var jump_power = 30
+export var gravity = 7.98
+export var jump_power = 200
 export var maxfallspeed = 200
-export var speed = 10
+export var speed = 5
 export var maxspeed = 10
 var movement = Vector2()
 
@@ -16,21 +16,30 @@ func _physics_process(delta):
 		
 		if Input.is_action_pressed("PlayerRight"):
 			movement.x = 0
+			$Sprite/Run.play("Idle")
 		else:
 			movement.x = -speed
 			
-	if Input.is_action_just_released("PlayerLeft"):
-		movement.x = lerp(movement.x,0,.01)
+			$Sprite/Run.play("WalkRight")
+			
+	if Input.is_action_just_released("PlayerLeft") or Input.is_action_just_released("PlayerRight"):
+		movement.x = 0
+		$Sprite/Run.play("Idle")
 		
 	if Input.is_action_pressed("PlayerRight"):
 		if Input.is_action_pressed("PlayerLeft"):
+			$Sprite/Run.play("Idle")
 			movement.x = 0
 		else:
 			movement.x = speed
+			$Sprite/Run.play("WalkLeft")
 	
 	
 	if Input.is_action_pressed("PlayerJump") and is_on_floor():
-		velocity.y += -jump_power
+		velocity.y -= jump_power
+		
+		
+		
 	if velocity.y < maxfallspeed:
 		velocity.y += gravity
 	else:
